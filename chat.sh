4ee0b785f919ae0e070db4fb4012e449f4bd3b41
Pranav Gade <pranavgade20@gmail.com>
2023-04-07 13:51:48 +0530

    Add files via upload

--- chat.sh
+++ chat.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-
 messages=$(echo '[]' | jq)
+echo -ne "$YELLOW""Welcome to a CLI for interacting with GPT chat models. Press Ctrl-D to send your input to the model, and Ctrl-C to exit $NC\n\n"
 echo -ne "$GREEN""User: $NC"
 
-while IFS='$\n' read -r line; do
+while line=$(cat); do
     messages=$(echo "$messages" | jq -c '. += [{"role": "user", "content": '"$(echo "$line" | jq -sR .)"'}]')
     response=''
     echo -ne "$RED""GPT: $NC"
 
-    curl --no-buffer -s -X POST -H 'Content-Type: application/json' -H 'Accept: text/event-stream' -H 'Authorization: Bearer '"$OPENAI_API_KEY" -d '{"model": "'"${CHAT_MODEL_NAME:="gpt-3.5-turbo"}"'", "messages": '"$messages"', "stream": true}' https://api.openai.com/v1/chat/completions > >(while read -r resp; do
+    curl --no-buffer -s -X POST -H 'Content-Type: application/json' -H 'Accept: text/event-stream' -H 'Authorization: Bearer '"$OPENAI_API_KEY" -d '{"model": "'"${CHAT_MODEL_NAME:="gpt-4"}"'", "messages": '"$messages"', "stream": true}' https://api.openai.com/v1/chat/completions > >(while read -r resp; do
         if [[ $resp == *"delta\":{\"content"* ]]; then
-            content=$(echo "$resp" | cut -c 7- | jq -r '.choices[].delta.content')
+		content="$(echo -E "$resp" | cut -c 7- | jq '.choices[].delta.content' | sed "s/^\"\|\"$//g")"
             echo -ne "$content"
             response+=$content
         fi
